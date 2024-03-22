58f09de3d5c3ab10e4361e987a2407286c8572d6
Artem0904 <moonportal708@gmail.com>
2024-03-22 20:40:37 +0200

    fix bugs

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,21 +1,32 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITask } from '../task';
 import { DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
-  imports: [LowerCasePipe, UpperCasePipe, DatePipe]
+  imports: [LowerCasePipe, UpperCasePipe, DatePipe, ReactiveFormsModule]
 })
 export class TodoListComponent {
+  
   @Input()
   task: ITask = {
     id: 0,
     description: "",
     status: false
   };
+
+  creationForm = this.fb.group({
+    id: [this.task.id],
+    description: [this.task.description],
+    status: [this.task.status],
+    date: [this.task.date],
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   @Output() removeEvent = new EventEmitter<number>();
 
